f0bef39c3698233da75e1d279bc296da05a8a3b1
Lucas Viana <lucaasviana280@gmail.com>
2021-07-28 20:18:12 -0300

    feature: instalando nodemon como devDependencie

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http'),
     app = express()
 
 app.get('/', (req, res) => {
-    res.send('Você está na Home!')
+    res.send('<h1>VOCÊ ESTÁ NA HOME PAGE!</h1>')
 })    
 
 app.listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
