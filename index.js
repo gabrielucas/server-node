4fe19bd1814e6adeb2beadee19a52ba665e9eb95
Lucas Viana <lucaasviana280@gmail.com>
2021-07-28 20:57:34 -0300

    feature: trabalhando com rotas e parâmetros

--- index.js
+++ index.js
@@ -6,6 +6,44 @@ app.get('/', (req, res) => {
     res.send('<h1>VOCÊ ESTÁ NA HOME PAGE!</h1>')
 })    
 
+app.get('/sobre', (req, res) => {
+    res.send('<h1>VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
+})
+
+app.get('/produto', (req,res) => {
+    res.send('<h1>VOCÊ ESTÁ NA PÁGINA DE PRODUTO</h1>')
+})
+
+app.get('/objeto', (req,res) => {
+    res.send({nome: "Lucas",
+sobrenome: "Viana",
+idade: 22,
+profissao: "Analista de Sistemas"})
+})
+
+app.get('/json', (req,res) => {
+    res.send({"array": [{nome: "Lucas",
+    sobrenome: "Viana",
+    idade: 22,
+    profissao: "Analista de Sistemas"},
+
+    {nome: "Pedro",
+    sobrenome: "Viana",
+    idade: 6,
+    profissao: ""}]})
+})
+
+app.get('/produto/:id', (req, res) => {
+    res.send(`<h1>VOCÊ ESTÁ NA PÁGINA DO PRODUTO DE ID ${req.params.id}</h1>`)
+})
+
+app.get('/usuarios/:nome?', (req, res) => {
+
+    const {nome} = req.params
+    res.send(nome ? `<h1>PÁGINA DO USUÁRIO ${nome.toUpperCase()}</h1>` : `<h1>PÁGINA DE USUÁRIOS</h1>`)
+
+})
+
 app.listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
 
 // http.createServer((req, res) => {
